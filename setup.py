94b830a843a07101f307f71954391d7bf5922011
omid-b <omid.bagherpur@gmail.com>
2022-05-10 12:59:12 -0400

    modify cythinize extention method

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from setuptools import setup
+from setuptools import Extension
 from Cython.Build import cythonize
 from setuptools.command.build_ext import build_ext as _build_ext
 
@@ -10,11 +11,29 @@ class build_ext(_build_ext):
         import numpy
         self.include_dirs.append(numpy.get_include())
 
+# extensions = [
+#     Extension("_dat", ["src/gdp/_dat.pyx"]),
+#     Extension("_geographic", ["src/gdp/_geographic.pyx"]),
+# ]
+
+extensions = [
+    Extension("_dat",
+              ["src/gdp/_dat.pyx"],
+              py_limited_api=True,
+              define_macros=[('CYTHON_LIMITED_API', '1')]
+             ),
+    Extension("_geographic",
+              ["src/gdp/_geographic.pyx"],
+              py_limited_api=True,
+              define_macros=[('CYTHON_LIMITED_API', '1')]
+             ),
+]
+
 setup(
     cmdclass={'build_ext':build_ext},
     setup_requires=['numpy','cython'],
     include_package_data=True,
-    ext_modules=cythonize(["src/gdp/_dat.pyx", "src/gdp/_geographic.pyx"]),
+    ext_modules=cythonize(extensions)
 )
 
 
